895cdf895743e1960d1b3772ddbd4f379949ab4d
Preeti Sehgal <preetisehgal2001@gmail.com>
2020-04-20 00:40:01 -0400

    created a rectangle class and a square class

--- shapes.py
+++ shapes.py
@@ -2,23 +2,30 @@ class rectangle:
     def __init__(self, length, width):
         self.length = length
         self.width = width
-    
+
     def area(self):
         rectarea = self.length * self.width
         return rectarea
 
     def perimeter(self):
-        rectperimiter = 2*self.length + 2*self.width
+        rectperimiter = 2 * self.length + 2 * self.width
         return rectperimiter
 
+
 class square(rectangle):
-    pass
+    def __init__(self, length):
+        super().__init__(length, length)
 
 
-rect = rectangle(5,10)
+rect = rectangle(5, 10)
 print('Area of rectangle - ', rect.area())
 
 print('Perimeter of rectangle - ', rect.perimeter())
 
-sqr = square(5,5)
+sqr = square(5)
 print('Are of a square - ', sqr.area())
+
+
+
+
+#
